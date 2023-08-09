a629f955edbb66546842e9c79c3ec446ec358f7a
Imesh <imeshisuranga00@gmail.com>
2023-08-10 00:36:54 +0530

    test

---
+++ YourProject/src/constants/SiteDetails.js
@@ -0,0 +1,12 @@
+export default {
+	app_key: 'MTVkemkxcG9jOGptM3ZpZDJiMnlmaQXXX',
+
+	url: 'https://yeroku.com',
+	terms_page: 'https://yeroku.com/help-center/',
+}
+
+export const defaultLanguage = {
+	code: 'en',
+	rtl: false
+}
+export const defaultCurrency = 'USD';
